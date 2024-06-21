bd38037413d324421cf1709ccca3085e16d10206
Franco De Lorenzi <francodl646@gmail.com>
2024-06-21 17:12:06 -0300

    Agrega paginación

--- nasa_image_gallery/views.py
+++ nasa_image_gallery/views.py
@@ -1,5 +1,6 @@
 # capa de vista/presentación
 # si se necesita algún dato (lista, valor, etc), esta capa SIEMPRE se comunica con services_nasa_image_gallery.py
+import math
 
 from django.shortcuts import redirect, render
 from .layers.services import services_nasa_image_gallery
@@ -20,10 +21,22 @@ def getAllImagesAndFavouriteList(request):  #agregado por facu 20/06
 
 # función principal de la galería.
 def home(request):
+    Q_IMAGES_PER_PAGE = 10 # define la cantidad de imagagenes que va a haber por cada pagina
     # llama a la función auxiliar getAllImagesAndFavouriteList() y obtiene 2 listados: uno de las imágenes de la API y otro de favoritos por usuario*.
     # (*) este último, solo si se desarrolló el opcional de favoritos; caso contrario, será un listado vacío [].
     images, favourite_list = getAllImagesAndFavouriteList(request)  #acá me esta dando las imagenes para mostrar en el inicio
-    return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list})
+    
+    page = int(request.GET.get('page', '') or 1) # obtiene la pagina actual que es pasada a traves de un query, en caso de que no esté definido toma 1
+    q_pages = math.ceil(len(images) / Q_IMAGES_PER_PAGE) # define la cantidad de paginas, dividiendo la cantidad de images por la cantidad de imagenes que deben mostrarse en cada pagina y rendondea para arriba
+
+    filtered_images = []
+    for i in range(Q_IMAGES_PER_PAGE * (page - 1), Q_IMAGES_PER_PAGE * (page - 1) + Q_IMAGES_PER_PAGE): 
+        if i < len(images) and not i > len(images): # Si "i" está dentro del len(images) agrega la imagen[i] a la lista vacia
+            filtered_images.append(images[i])
+        else: # si está fuera del rango termina el ciclo
+            break
+
+    return render(request, 'home.html', {'images': filtered_images, 'favourite_list': favourite_list, 'q_pages': range(1, q_pages + 1)})
 
 # función utilizada en el buscador.
 def search(request):
